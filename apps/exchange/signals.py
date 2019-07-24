d8356ac8649279a3f091bf82fc2a05df8ae23aa8
oldseven <oldseven@oldseven.com>
2019-07-24 14:09:58 +0800

    FK

--- apps/exchange/signals.py
+++ apps/exchange/signals.py
@@ -1,15 +1,27 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from .models import ThumbsUp
+from .utils import get_object_or_none
 
 
-@receiver(post_save, sender=ThumbsUp)
-def after_create_thumb_up(sender, instance, created, **kwargs):
-    if created:
-        store = instance.store
+@receiver(pre_save, sender=ThumbsUp)
+def after_create_thumb_up(sender, instance, **kwargs):
+    old_instance = get_object_or_none(ThumbsUp, pk=instance.pk)
+    store = instance.store
+
+    if old_instance && old_instance.like != instance.like:
+        if instance.like:
+            store.likes += 1
+            store.dislikes -= 1
+        else:
+            store.dislikes += 1
+            store.likes -= 1
+    else:
         if instance.like:
             store.likes += 1
         else:
             store.dislikes += 1
-        store.save() 
+
+    store.save()
+
 
